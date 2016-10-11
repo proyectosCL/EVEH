161701947a6cecc6a43cc46b46b2bc661c5e33ac
BastianLillo <bast.lillo@alumnos.duoc.cl>
2016-10-11 13:07:17 -0300

    Agregar componente en progreso

--- EscuelaDeVuelo/src/Controlador/Administrar_Componente.java
+++ EscuelaDeVuelo/src/Controlador/Administrar_Componente.java
@@ -1,8 +1,29 @@
 package Controlador;
+import Database.Conexion;
+import Modelo.Componente;
+
+
 
 public class Administrar_Componente implements administrar_horas_vuelo {
 
-    public void ingresarComponente() {
+    public boolean ingresarComponente(Componente nuevoComponente) {
+        
+        String desc = nuevoComponente.getDescripcion();
+        String fabricte = nuevoComponente.getFabricante();
+        Float horasVuelo = nuevoComponente.getHoras_vuelo();
+        int diasVuelo = nuevoComponente.getDias_vuelo();
+        
+        Conexion conec = new Conexion();
+        conec.conectar();
+       // String sql=
+       //conec.escribir(sql);
+            
+            return true;
+        //} catch (Exception e) {
+        //    return false;
+        //}
+        
+        
     }
 
     public void asociarAeronave() {
