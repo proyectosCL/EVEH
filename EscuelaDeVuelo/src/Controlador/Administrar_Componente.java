78e7183cf237d7eedbdb258920faf433d9bbf6cb
pcbla <pcbla@DESKTOP-P7ESPHE>
2016-10-15 06:15:15 -0300

    +Componentes

--- EscuelaDeVuelo/src/Controlador/Administrar_Componente.java
+++ EscuelaDeVuelo/src/Controlador/Administrar_Componente.java
@@ -1,29 +1,32 @@
 package Controlador;
+
 import Database.Conexion;
 import Modelo.Componente;
-
-
 
 public class Administrar_Componente implements administrar_horas_vuelo {
 
     public boolean ingresarComponente(Componente nuevoComponente) {
-        
-    //    String desc = nuevoComponente.getDescripcion();
-      //  String fabricte = nuevoComponente.getFabricante();
-        //Float horasVuelo = nuevoComponente.getHoras_vuelo();
-        //int diasVuelo = nuevoComponente.getDias_vuelo();
-        
-        Conexion conec = new Conexion();
-        conec.conectar();
-       // String sql=
-       //conec.escribir(sql);
-            
+        try {
+            String desc = nuevoComponente.getDescripcion();
+            String fabricte = nuevoComponente.getFabricante();
+            Float horasVuelo = nuevoComponente.getHoras_vuelo();
+            int diasVuelo = nuevoComponente.getDias_vuelo();
+            int tpoCompteId = nuevoComponente.getTipo_componente_id();
+            int compteId = nuevoComponente.getComponente_id();
+            int naveId = nuevoComponente.getAeronave_id();
+
+            Conexion conec = new Conexion();
+            conec.conectar();
+            String sql= "INSERT INTO componentes  VALUES ((select (max(id)+1)from componentes),"
+                    + "'" + desc + "','" + fabricte + "','" + horasVuelo + "','" + diasVuelo + "',"
+                    + "'" + tpoCompteId + "','" + compteId + "','" + naveId + "')";
+            conec.escribir(sql);
+
             return true;
-        //} catch (Exception e) {
-        //    return false;
-        //}
-        
-        
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
     public void asociarAeronave() {
@@ -32,8 +35,7 @@ public class Administrar_Componente implements administrar_horas_vuelo {
     public void asociarSubcomponente() {
     }
 
-    public void configurarComponente() {
-    }
+   
 
     @Override
     public void sumarHoras() {
--- EscuelaDeVuelo/src/Modelo/Componente.java
+++ EscuelaDeVuelo/src/Modelo/Componente.java
@@ -11,7 +11,87 @@ public class Componente {
     private Float horas_vuelo;
 
     private int dias_vuelo;
+    
+    private int tipo_componente_id;
+            
+    private int componente_id;
+            
+    private int aeronave_id;
 
     public Componente() {
     }
+
+    public Componente(int id, String descripcion, String fabricante, Float horas_vuelo, int dias_vuelo) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.fabricante = fabricante;
+        this.horas_vuelo = horas_vuelo;
+        this.dias_vuelo = dias_vuelo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public Float getHoras_vuelo() {
+        return horas_vuelo;
+    }
+
+    public void setHoras_vuelo(Float horas_vuelo) {
+        this.horas_vuelo = horas_vuelo;
+    }
+
+    public int getDias_vuelo() {
+        return dias_vuelo;
+    }
+
+    public void setDias_vuelo(int dias_vuelo) {
+        this.dias_vuelo = dias_vuelo;
+    }
+
+    public int getTipo_componente_id() {
+        return tipo_componente_id;
+    }
+
+    public void setTipo_componente_id(int tipo_componente_id) {
+        this.tipo_componente_id = tipo_componente_id;
+    }
+
+    public int getComponente_id() {
+        return componente_id;
+    }
+
+    public void setComponente_id(int componente_id) {
+        this.componente_id = componente_id;
+    }
+
+    public int getAeronave_id() {
+        return aeronave_id;
+    }
+
+    public void setAeronave_id(int aeronave_id) {
+        this.aeronave_id = aeronave_id;
+    }
+    
+    
 }
