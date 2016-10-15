821c941d57aaa50a5c04a5e66a57686ce99741c6
pcbla <pcbla@DESKTOP-P7ESPHE>
2016-10-15 08:35:28 -0300

    +Componentes

--- EscuelaDeVuelo/src/Controlador/Administrar_Componente.java
+++ EscuelaDeVuelo/src/Controlador/Administrar_Componente.java
@@ -2,6 +2,10 @@ package Controlador;
 
 import Database.Conexion;
 import Modelo.Componente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Administrar_Componente implements administrar_horas_vuelo {
 
@@ -26,6 +30,62 @@ public class Administrar_Componente implements administrar_horas_vuelo {
         } catch (Exception e) {
             return false;
         }
+
+    }
+    
+     public ArrayList<Componente> listarFuselaje() {
+         
+          ArrayList listaFuselaje = new ArrayList();
+        Componente compte;
+        try {
+            Conexion dbconn = new Conexion();
+            dbconn.conectar();
+            ResultSet rs = dbconn.consultar("SELECT * FROM componentes WHERE tipos_componentes_id =1");
+            while (rs.next()) {
+                compte = new Componente();
+                compte.setId(rs.getInt("id"));
+                compte.setDescripcion(rs.getString(2));
+                compte.setFabricante(rs.getString(3));
+                compte.setHoras_vuelo(rs.getFloat("horas_vuelo"));
+                compte.setDias_vuelo(rs.getInt("dias_vuelo"));
+                compte.setTipo_componente_id(rs.getInt("tipos_componentes_id"));
+                compte.setComponente_id(rs.getInt("componentes_id"));
+                compte.setAeronave_id(rs.getInt("aeronaves_id"));
+                listaFuselaje.add(compte);
+            }
+
+        } catch (Exception e) {
+
+        }
+        return listaFuselaje;
+
+    }
+     
+     public ArrayList<Componente> listarAlas() {
+         
+          ArrayList listaAlas = new ArrayList();
+        Componente compte;
+        try {
+            Conexion dbconn = new Conexion();
+            dbconn.conectar();
+            ResultSet rs = dbconn.consultar("SELECT * FROM componentes WHERE tipos_componentes_id =2");
+            while (rs.next()) {
+                compte = new Componente();
+                compte.setId(rs.getInt("id"));
+                compte.setDescripcion(rs.getString(2));
+                compte.setFabricante(rs.getString(3));
+                compte.setHoras_vuelo(rs.getFloat("horas_vuelo"));
+                compte.setDias_vuelo(rs.getInt("dias_vuelo"));
+                compte.setTipo_componente_id(rs.getInt("tipos_componentes_id"));
+                compte.setComponente_id(rs.getInt("componentes_id"));
+                compte.setAeronave_id(rs.getInt("aeronaves_id"));
+                listaAlas.add(compte);
+            }
+
+        } catch (Exception e) {
+
+        }
+        return listaAlas;
 
     }
 
