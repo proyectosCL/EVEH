29e26633a8e8b184a72d22295dec7b64ac1c7881
pcbla <pcbla@DESKTOP-P7ESPHE>
2016-10-29 08:33:39 -0300

    +Actualizacion subComponentes en proceso

--- EscuelaDeVuelo/src/Vista/IngresarSubComponente.java
+++ EscuelaDeVuelo/src/Vista/IngresarSubComponente.java
@@ -102,7 +102,7 @@ public class IngresarSubComponente extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnGuardarNuevoComponenteSub)
-                .addGap(263, 263, 263))
+                .addGap(306, 306, 306))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
